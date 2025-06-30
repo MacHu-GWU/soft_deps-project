4632eda3fc30cfb3140eb9b564b8dcd7dacf704b
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-06-30 16:15:45 -0400

    add example

---
+++ example/user_script.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from your_library import your_feature
+
+# this will raise ImportError:
+#   ImportError: You didn't install `important_library`. To use `awesome_tool`, please install it.
+your_feature()
